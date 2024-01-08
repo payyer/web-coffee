413d4696c7ce8fdad7528529a749127d7d377585
AnhHocBE <quocanhle112@gmail.com>
2024-01-08 23:06:01 +0700

    setup-base

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+function App() {
+
+  return (
+    <>
+      <header>
+        Header
+      </header>
+      <main>
+        <section>
+          Slider
+        </section>
+        <section>
+          Story
+        </section>
+        <section>
+          Fetured-mugs
+        </section>
+        <section>
+          More-product
+        </section>
+        <section>
+          Coffee Magazine
+        </section>
+        <section>
+          Life style story
+        </section>
+        <section>
+          Subscribe
+        </section>
+      </main>
+      <footer>
+        Footer
+      </footer>
+    </>
+  )
+}
+
+export default App
